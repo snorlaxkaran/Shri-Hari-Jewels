1eeca16b41f25ba276054b108af07408571e667d
snorlax.karan@gmail.com <snorlax.karan@gmail.com>
2026-06-11 21:39:54 +0530

    Built the basic structure

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const dashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className={`flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+      sidebard
+      <main
+        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}
+      >
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default dashboardWrapper;
